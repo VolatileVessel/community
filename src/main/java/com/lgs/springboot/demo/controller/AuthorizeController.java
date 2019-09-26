4686b9662072b643ad4a62cfd80bdff4da35c9d6
VolatileVessel <lgs287@126.com>
2019-09-26 14:30:42 +0800

    add github authorize

---
+++ src/main/java/com/lgs/springboot/demo/controller/AuthorizeController.java
@@ -0,0 +1,42 @@
+package com.lgs.springboot.demo.controller;
+
+import com.lgs.springboot.demo.DTO.AccessTokenDTO;
+import com.lgs.springboot.demo.DTO.GithubUser;
+import com.lgs.springboot.demo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientid;
+    @Value("${github.client.secret}")
+    private String clentSecret;
+    @Value("${github.clent.uri}")
+    private String clentUri;
+    //接收code和state的
+    @RequestMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code ,
+                            @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientid);
+        accessTokenDTO.setClient_secret(clentSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(clentUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/lgs/springboot/demo/controller/Hello.java
+++ src/main/java/com/lgs/springboot/demo/controller/Hello.java
@@ -1,20 +1,27 @@
 package com.lgs.springboot.demo.controller;
 
+import com.lgs.springboot.demo.domain.City;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class Hello {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    @ResponseBody
-    public String SayHello(){
+    //@ResponseBody
+    public String SayHello(City city, HttpServletRequest request){
 		System.out.println("hello word11");
 		System.out.println("hello word2");
-        System.out.print("1");
-        System.out.println("hello word2");
-        return "Hello,SpringBoot!";
+		city.setId(1L);
+		city.setCityName("湖北");
+		city.setDescription("湖北描述");
+		city.setProvinceId(11L);
+        request.setAttribute(city.getCityName(),"cityName");
+
+        return "index";
     }
 
 }
